0526d8662a16becdfac2e3629338c3efcb2a82f5
arielmell0 <arielmello@rede.ulbra.br>
2022-04-28 06:56:17 -0300

    feat: Criando a primeira página e seus respectivos testes

---
+++ src/pages/app/App.js
@@ -0,0 +1,11 @@
+import devilCatImg from '../../images/devil-cat.png';
+
+export function App() {
+    return (
+        <div>
+            <p>memes de gatinho</p>
+            <button>Fazer Reserva</button>
+            <img src={devilCatImg} alt="Meme do gatinho"/>
+        </div>
+    )
+}
---
+++ src/pages/app/App.test.js
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { App } from './App';
+
+test('renders the app, with a button', () => {
+    render(<App />);
+
+    const buttonEl = screen.getByRole('button');
+    const imageEl = screen.getByRole('img');
+    const textEl = screen.getByText(/memes/);
+
+    expect(buttonEl).toBeInTheDocument();
+    expect(imageEl).toBeInTheDocument();
+    expect(textEl).toBeInTheDocument();
+})
