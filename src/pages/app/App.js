0b8a7420a605d975c4d7e518b96cd23b90298288
arielmell0 <arielmello@rede.ulbra.br>
2022-05-01 23:33:56 -0300

    feat: >Evoluindo nosso projeto através do controle de estado
    usando useStates

--- src/pages/app/App.js
+++ src/pages/app/App.js
@@ -1,11 +1,25 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 import narutoImg from '../../images/naruto.png';
 import { Quotes } from '../../components';
+import { getQuote } from '../../services';
 
 export function App() {
+    const [quoteState, setQuoteState ] = useState({ quote: 'ok', speaker: 'Speaker' });
+
+    const onUpdate = async () => {
+        const quote = await getQuote();
+
+        setQuoteState(quote);
+    };
+
     return (
         <Content>
-            <Quotes quote={'ok'} speaker={'Speaker'}/>
+            <Quotes
+                // quote={quoteState.quote} 
+                // speaker={quoteState.speaker}   ou
+                {...quoteState}
+                onUpdate={onUpdate}/>
             <NarutoImg src={narutoImg} alt="Naruto com uma kunai"/>
         </Content>
     );
--- src/pages/app/App.test.js
+++ src/pages/app/App.test.js
@@ -1,6 +1,19 @@
-import { render, screen } from '@testing-library/react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { App } from './App';
+
+const response = {
+    speaker:'Speaker',
+    quote: 'test quote'
+}
+
+const server = setupServer(
+    rest.get(process.env.REACT_APP_API, (req, res, ctx) => {
+        return res(ctx.json(response));
+    })
+)
 
 test('renders the app, with a button', () => {
     render(<App />);
@@ -12,4 +25,16 @@ test('renders the app, with a button', () => {
     expect(buttonEl).toBeInTheDocument();
     expect(imageEl).toBeInTheDocument();
     expect(textEl).toBeInTheDocument();
-})
+});
+
+test('calls api on button click and update its text', async () => {
+    render(<App />);
+
+    const buttonEl = screen.getByRole('button');
+
+    fireEvent.click(buttonEl);
+
+    const quoteEl = await screen.findByText(response.quote);
+
+    expect(quoteEl).toBeInTheDocument();
+});
